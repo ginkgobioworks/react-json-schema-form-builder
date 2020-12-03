nathanalam/formbuildertesting

Port over Ginkgo Form Builder

src/formBuilder/CardEnumOptions.js
@@ -1,0 +1,116 @@
+// @flow
+
+import * as React from 'react';
+import { Input } from 'reactstrap';
+
+// Input field corresponding to an array of values, add and remove
+export default function CardEnumOptions({
+  initialValues,
+  names,
+  showNames,
+  onChange,
+  type,
+}: {
+  initialValues: Array<any>,
+  names?: Array<string>,
+  showNames: boolean,
+  onChange: (newEnums: Array<any>, newEnumNames?: Array<string>) => void,
+  type: string,
+}) {
+  const possibleValues = [];
+  for (let index = 0; index < initialValues.length; index += 1) {
+    const value = initialValues[index];
+    let name = `${value}`;
+    if (names && index < names.length) name = names[index];
+    possibleValues.push(
+      <div key={index} className="card-enum-option">
+        <Input
+          value={value === undefined || value === null ? '' : value}
+          placeholder="Possible Value"
+          key={`val-${index}`}
+          type={type === 'string' ? 'text' : 'number'}
+          onChange={(ev: any) => {
+            let newVal;
+            switch (type) {
+              case 'string':
+                newVal = ev.target.value;
+                break;
+              case 'number':
+              case 'integer':
+                newVal = parseFloat(ev.target.value);
+                if (Number.isInteger(newVal))
+                  newVal = parseInt(ev.target.value, 10);
+                if (Number.isNaN(newVal)) newVal = null;
+                break;
+              default:
+                throw new Error(`Enum called with unknown type ${type}`);
+            }
+            onChange(
+              [
+                ...initialValues.slice(0, index),
+                newVal,
+                ...initialValues.slice(index + 1),
+              ],
+              names
+            );
+          }}
+          className="card-text"
+        />
+        <Input
+          value={name || ''}
+          placeholder="Name"
+          key={`name-${index}`}
+          type="text"
+          onChange={(ev: any) => {
+            if (names)
+              onChange(initialValues, [
+                ...names.slice(0, index),
+                ev.target.value,
+                ...names.slice(index + 1),
+              ]);
+          }}
+          className="card-text"
+          style={{ display: showNames ? 'initial' : 'none' }}
+        />
+        <i
+          className="fa fa-trash"
+          onClick={() => {
+            // remove this value
+            onChange(
+              [
+                ...initialValues.slice(0, index),
+                ...initialValues.slice(index + 1),
+              ],
+              names
+                ? [...names.slice(0, index), ...names.slice(index + 1)]
+                : undefined
+            );
+          }}
+        ></i>
+      </div>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <div className="card-enum-header">
+        <p> Value </p>
+        <p style={{ display: showNames ? 'initial' : 'none' }}>
+          {' '}
+          Display Label{' '}
+        </p>
+      </div>
+      {possibleValues}
+      <i
+        className="fa fa-plus"
+        onClick={() => {
+          // add a new dropdown option
+          onChange(
+            [...initialValues, null],
+            names ? [...names, ''] : undefined
+          );
+        }}
+      ></i>
+    </React.Fragment>
+  );
+}

src/formBuilder/PreDefinedGallery.js
@@ -1,0 +1,228 @@
+// @flow
+import * as React from 'react';
+import { createUseStyles } from 'react-jss';
+import CardGallery from './CardGallery';
+import {
+  parse,
+  stringify,
+  propagateDefinitionChanges,
+  generateCategoryHash,
+} from './utils';
+import type { Mods } from './types';
+
+const useStyles = createUseStyles({
+  preDefinedGallery: {
+    display: 'flex',
+    'flex-direction': 'column',
+    'text-align': 'center',
+    '& i': {
+      cursor: 'pointer',
+    },
+    '& .fa-question-circle': {
+      color: 'gray',
+    },
+    '& .fa-asterisk': {
+      'font-size': '.9em',
+      color: 'green',
+    },
+    '& .fa-plus-square': {
+      color: 'green',
+      'font-size': '1.5em',
+      margin: '0 auto',
+    },
+    '& .card-container': {
+      '&:hover': {
+        border: '1px solid green',
+      },
+      width: '70%',
+      'min-width': '400px',
+      margin: '2em auto',
+      border: '1px solid gray',
+      'border-radius': '4px',
+      'background-color': 'white',
+      '& .toggle-collapse': {
+        margin: '0.25em .5em 0 .5em !important',
+      },
+      '& h4': {
+        width: '100%',
+        'text-align': 'left',
+        display: 'inline-block',
+        color: '#138AC2',
+        margin: '0.25em .5em 0 .5em',
+        'font-size': '18px',
+      },
+      '& .d-flex': {
+        'border-bottom': '1px solid gray',
+      },
+      '& .label': {
+        float: 'left',
+      },
+      '& .arrows': {
+        'text-align': 'right',
+        float: 'right',
+        '& .fa-trash': {
+          border: '1px solid #DE5354',
+          color: '#DE5354',
+        },
+        '& .fa': {
+          'border-radius': '4px',
+          padding: '.25em',
+          margin: '0 .5em 0 0',
+        },
+      },
+    },
+    '& .card-requirements': {
+      border: '1px dashed black',
+    },
+    '& .section-container': {
+      '&:hover': {
+        border: '1px solid green',
+      },
+      display: 'block',
+      width: '90%',
+      'min-width': '400px',
+      margin: '2em auto',
+      border: '1px solid var(--gray)',
+      'border-radius': '4px',
+      'background-color': 'white',
+      '& .toggle-collapse': {
+        margin: '0.25em .5em 0 .5em !important',
+      },
+      '& h4': {
+        width: '100%',
+        'text-align': 'left',
+        display: 'inline-block',
+        color: '#138AC2',
+        margin: '0.25em .5em 0 .5em',
+        'font-size': '18px',
+      },
+      '& .d-flex': {
+        'border-bottom': '1px solid var(--gray)',
+      },
+      '& .label': {
+        float: 'left',
+      },
+      '& .arrows': {
+        'text-align': 'right',
+        float: 'right',
+        '& .fa-trash': {
+          border: '1px solid #DE5354',
+          color: '#DE5354',
+        },
+        '& .fa': {
+          'border-radius': '4px',
+          padding: '.25em',
+          margin: '0 .5em 0 0',
+        },
+      },
+    },
+    '& .section-dependent': {
+      border: '1px dashed gray',
+    },
+    '& .section-requirements': {
+      border: '1px dashed black',
+    },
+    '& .fa-pencil, & .fa-arrow-up, & .fa-arrow-down': {
+      border: '1px solid #1d71ad',
+      color: '#1d71ad',
+    },
+    '& .modal-body': {
+      maxHeight: '500px',
+      overflowY: 'scroll',
+    },
+    '& .card-container:hover': { border: '1px solid var(--green)' },
+    '& .card-dependent': { border: '1px dashed var(--gray)' },
+    '& .card-add': {
+      cursor: 'pointer',
+      display: 'block',
+      color: '$green',
+      fontSize: '1.5em',
+    },
+
+    '& .section-container:hover': { border: '1px solid var(--green)' },
+  },
+});
+
+export default function PreDefinedGallery({
+  schema,
+  uischema,
+  onChange,
+  lang,
+  mods,
+}: {
+  schema: string,
+  uischema: string,
+  onChange: (string, string) => any,
+  lang: string,
+  mods?: Mods,
+}) {
+  const classes = useStyles();
+  const schemaData = (parse(schema, lang): { [string]: any }) || {};
+  const uiSchemaData = (parse(uischema, lang): { [string]: any }) || {};
+  const categoryHash = generateCategoryHash(mods);
+
+  React.useEffect(() => {
+    if (!uiSchemaData.definitions) {
+      // eslint-disable-next-line no-console
+      console.log('Parsing UI schema to assign UI for definitions');
+      // need to create definitions from scratch
+      const references = [];
+      // recursively search for all $refs in the schemaData
+      const findRefs = (name, schemaObject) => {
+        if (!schemaObject) return;
+        if (typeof schemaObject === 'object')
+          Object.keys(schemaObject).forEach((key) => {
+            if (typeof key === 'string') {
+              if (key === '$ref') references.push(name);
+              findRefs(key, schemaObject[key]);
+            }
+          });
+        if (Array.isArray(schemaObject))
+          schemaObject.forEach((innerObj) => {
+            findRefs(name, innerObj);
+          });
+      };
+
+      findRefs('root', schemaData);
+
+      uiSchemaData.definitions = {};
+      const referenceSet = new Set(references);
+      Object.keys(uiSchemaData).forEach((uiProp) => {
+        if (referenceSet.has(uiProp))
+          uiSchemaData.definitions[uiProp] = uiSchemaData[uiProp];
+      });
+
+      onChange(stringify(schemaData, lang), stringify(uiSchemaData, lang));
+    }
+  }, [uischema, schema]);
+  return (
+    <div className={classes.preDefinedGallery}>
+      <CardGallery
+        definitionSchema={schemaData.definitions}
+        definitionUiSchema={uiSchemaData.definitions}
+        language={lang}
+        onChange={(
+          newDefinitions: { [string]: any },
+          newDefinitionsUi: { [string]: any }
+        ) => {
+          schemaData.definitions = newDefinitions;
+          uiSchemaData.definitions = newDefinitionsUi;
+          // propagate changes in ui definitions to all relavant parties in main schema
+
+          propagateDefinitionChanges(
+            schemaData,
+            uiSchemaData,
+            (newSchema, newUiSchema) =>
+              onChange(
+                stringify(newSchema, lang),
+                stringify(newUiSchema, lang)
+              ),
+            categoryHash
+          );
+        }}
+        mods={mods}
+        categoryHash={categoryHash}
+      />
+    </div>
+  );
+}

ginkgobioworks/react-json-schema-form-builder

src/formBuilder/CardEnumOptions.js
@@ -1,0 +1,116 @@
+// @flow
+
+import * as React from 'react';
+import { Input } from 'reactstrap';
+
+// Input field corresponding to an array of values, add and remove
+export default function CardEnumOptions({
+  initialValues,
+  names,
+  showNames,
+  onChange,
+  type,
+}: {
+  initialValues: Array<any>,
+  names?: Array<string>,
+  showNames: boolean,
+  onChange: (newEnums: Array<any>, newEnumNames?: Array<string>) => void,
+  type: string,
+}) {
+  const possibleValues = [];
+  for (let index = 0; index < initialValues.length; index += 1) {
+    const value = initialValues[index];
+    let name = `${value}`;
+    if (names && index < names.length) name = names[index];
+    possibleValues.push(
+      <div key={index} className="card-enum-option">
+        <Input
+          value={value === undefined || value === null ? '' : value}
+          placeholder="Possible Value"
+          key={`val-${index}`}
+          type={type === 'string' ? 'text' : 'number'}
+          onChange={(ev: any) => {
+            let newVal;
+            switch (type) {
+              case 'string':
+                newVal = ev.target.value;
+                break;
+              case 'number':
+              case 'integer':
+                newVal = parseFloat(ev.target.value);
+                if (Number.isInteger(newVal))
+                  newVal = parseInt(ev.target.value, 10);
+                if (Number.isNaN(newVal)) newVal = type === 'string' ? '' : 0;
+                break;
+              default:
+                throw new Error(`Enum called with unknown type ${type}`);
+            }
+            onChange(
+              [
+                ...initialValues.slice(0, index),
+                newVal,
+                ...initialValues.slice(index + 1),
+              ],
+              names
+            );
+          }}
+          className="card-text"
+        />
+        <Input
+          value={name || ''}
+          placeholder="Name"
+          key={`name-${index}`}
+          type="text"
+          onChange={(ev: any) => {
+            if (names)
+              onChange(initialValues, [
+                ...names.slice(0, index),
+                ev.target.value,
+                ...names.slice(index + 1),
+              ]);
+          }}
+          className="card-text"
+          style={{ display: showNames ? 'initial' : 'none' }}
+        />
+        <i
+          className="fa fa-trash"
+          onClick={() => {
+            // remove this value
+            onChange(
+              [
+                ...initialValues.slice(0, index),
+                ...initialValues.slice(index + 1),
+              ],
+              names
+                ? [...names.slice(0, index), ...names.slice(index + 1)]
+                : undefined
+            );
+          }}
+        ></i>
+      </div>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <div className="card-enum-header">
+        <p> Value </p>
+        <h5 style={{ display: showNames ? 'initial' : 'none' }}>
+          {' '}
+          Display Label{' '}
+        </h5>
+      </div>
+      {possibleValues}
+      <i
+        className="fa fa-plus"
+        onClick={() => {
+          // add a new dropdown option
+          onChange(
+            [...initialValues, type === 'string' ? '' : 0],
+            names ? [...names, ''] : undefined
+          );
+        }}
+      ></i>
+    </React.Fragment>
+  );
+}

src/formBuilder/PreDefinedGallery.js
@@ -1,0 +1,238 @@
+// @flow
+import * as React from 'react';
+import { createUseStyles } from 'react-jss';
+import CardGallery from './CardGallery';
+import {
+  parse,
+  stringify,
+  propagateDefinitionChanges,
+  generateCategoryHash,
+} from './utils';
+import DEFAULT_FORM_INPUTS from './defaults/defaultFormInputs';
+import type { Mods } from './types';
+
+const useStyles = createUseStyles({
+  preDefinedGallery: {
+    display: 'flex',
+    'flex-direction': 'column',
+    'text-align': 'center',
+    '& i': {
+      cursor: 'pointer',
+    },
+    '& .fa-question-circle': {
+      color: 'gray',
+    },
+    '& .fa-asterisk': {
+      'font-size': '.9em',
+      color: 'green',
+    },
+    '& .form_footer': {
+      marginTop: '1em',
+      textAlign: 'center',
+      '& i': { cursor: 'pointer', color: '$green', fontSize: '1.5em' },
+    },
+    '& .fa-plus-square': {
+      color: 'green',
+      'font-size': '1.5em',
+      margin: '0 auto',
+    },
+    '& .card-container': {
+      '&:hover': {
+        border: '1px solid green',
+      },
+      width: '70%',
+      'min-width': '400px',
+      margin: '2em auto',
+      border: '1px solid gray',
+      'border-radius': '4px',
+      'background-color': 'white',
+      '& .toggle-collapse': {
+        margin: '0.25em .5em 0 .5em !important',
+      },
+      '& h4': {
+        width: '100%',
+        'text-align': 'left',
+        display: 'inline-block',
+        color: '#138AC2',
+        margin: '0.25em .5em 0 .5em',
+        'font-size': '18px',
+      },
+      '& .d-flex': {
+        'border-bottom': '1px solid gray',
+      },
+      '& .label': {
+        float: 'left',
+      },
+      '& .arrows': {
+        'text-align': 'right',
+        float: 'right',
+        '& .fa-trash': {
+          border: '1px solid #DE5354',
+          color: '#DE5354',
+        },
+        '& .fa': {
+          'border-radius': '4px',
+          padding: '.25em',
+          margin: '0 .5em 0 0',
+        },
+      },
+    },
+    '& .card-requirements': {
+      border: '1px dashed black',
+    },
+    '& .section-container': {
+      '&:hover': {
+        border: '1px solid green',
+      },
+      display: 'block',
+      width: '90%',
+      'min-width': '400px',
+      margin: '2em auto',
+      border: '1px solid var(--gray)',
+      'border-radius': '4px',
+      'background-color': 'white',
+      '& .toggle-collapse': {
+        margin: '0.25em .5em 0 .5em !important',
+      },
+      '& h4': {
+        width: '100%',
+        'text-align': 'left',
+        display: 'inline-block',
+        color: '#138AC2',
+        margin: '0.25em .5em 0 .5em',
+        'font-size': '18px',
+      },
+      '& .d-flex': {
+        'border-bottom': '1px solid var(--gray)',
+      },
+      '& .label': {
+        float: 'left',
+      },
+      '& .arrows': {
+        'text-align': 'right',
+        float: 'right',
+        '& .fa-trash': {
+          border: '1px solid #DE5354',
+          color: '#DE5354',
+        },
+        '& .fa': {
+          'border-radius': '4px',
+          padding: '.25em',
+          margin: '0 .5em 0 0',
+        },
+      },
+    },
+    '& .section-dependent': {
+      border: '1px dashed gray',
+    },
+    '& .section-requirements': {
+      border: '1px dashed black',
+    },
+    '& .fa-pencil, & .fa-arrow-up, & .fa-arrow-down': {
+      border: '1px solid #1d71ad',
+      color: '#1d71ad',
+    },
+    '& .modal-body': {
+      maxHeight: '500px',
+      overflowY: 'scroll',
+    },
+    '& .card-container:hover': { border: '1px solid var(--green)' },
+    '& .card-dependent': { border: '1px dashed var(--gray)' },
+    '& .card-add': {
+      cursor: 'pointer',
+      display: 'block',
+      color: '$green',
+      fontSize: '1.5em',
+    },
+
+    '& .section-container:hover': { border: '1px solid var(--green)' },
+  },
+});
+
+export default function PreDefinedGallery({
+  schema,
+  uischema,
+  onChange,
+  lang,
+  mods,
+}: {
+  schema: string,
+  uischema: string,
+  onChange: (string, string) => any,
+  lang: string,
+  mods?: Mods,
+}) {
+  const classes = useStyles();
+  const schemaData = (parse(schema, lang): { [string]: any }) || {};
+  const uiSchemaData = (parse(uischema, lang): { [string]: any }) || {};
+  const allFormInputs = {
+    ...DEFAULT_FORM_INPUTS,
+    ...(mods && mods.customFormInputs),
+  };
+  const categoryHash = generateCategoryHash(allFormInputs);
+
+  React.useEffect(() => {
+    if (!uiSchemaData.definitions) {
+      // eslint-disable-next-line no-console
+      console.log('Parsing UI schema to assign UI for definitions');
+      // need to create definitions from scratch
+      const references = [];
+      // recursively search for all $refs in the schemaData
+      const findRefs = (name, schemaObject) => {
+        if (!schemaObject) return;
+        if (typeof schemaObject === 'object')
+          Object.keys(schemaObject).forEach((key) => {
+            if (typeof key === 'string') {
+              if (key === '$ref') references.push(name);
+              findRefs(key, schemaObject[key]);
+            }
+          });
+        if (Array.isArray(schemaObject))
+          schemaObject.forEach((innerObj) => {
+            findRefs(name, innerObj);
+          });
+      };
+
+      findRefs('root', schemaData);
+
+      uiSchemaData.definitions = {};
+      const referenceSet = new Set(references);
+      Object.keys(uiSchemaData).forEach((uiProp) => {
+        if (referenceSet.has(uiProp))
+          uiSchemaData.definitions[uiProp] = uiSchemaData[uiProp];
+      });
+
+      onChange(stringify(schemaData, lang), stringify(uiSchemaData, lang));
+    }
+  }, [uischema, schema]);
+  return (
+    <div className={classes.preDefinedGallery}>
+      <CardGallery
+        definitionSchema={schemaData.definitions}
+        definitionUiSchema={uiSchemaData.definitions}
+        language={lang}
+        onChange={(
+          newDefinitions: { [string]: any },
+          newDefinitionsUi: { [string]: any }
+        ) => {
+          schemaData.definitions = newDefinitions;
+          uiSchemaData.definitions = newDefinitionsUi;
+          // propagate changes in ui definitions to all relavant parties in main schema
+
+          propagateDefinitionChanges(
+            schemaData,
+            uiSchemaData,
+            (newSchema, newUiSchema) =>
+              onChange(
+                stringify(newSchema, lang),
+                stringify(newUiSchema, lang)
+              ),
+            categoryHash
+          );
+        }}
+        mods={mods}
+        categoryHash={categoryHash}
+      />
+    </div>
+  );
+}