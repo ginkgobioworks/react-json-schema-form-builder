d76b2f6b70cd91280566c990459ed4d03f87e8c6
Raymond Lam <ray@lam-ray.com>
2020-12-21 23:27:25 -0500

    Fix several issues with the flow-libdef
    Fix several issues with the flow-libdef
    - Remove `CardProps`, `SectionProps`, and `ElementProps`, which were vestigial and not currently being used.
    - Remove exactness in object definitions with indexer properties, as they are incompatible.
    - Export a props type for `cardBody` and `modalBody` components so that these components can be defined externally.
    - Use `React$ComponentType`, as `React$AbstractComponent` was incorrect.

--- flow-libdef/@ginkgo-bioworks/react-json-schema-form-builder_v1.x.x/test_react-json-schema-form-builder_v1.x.x.js
+++ flow-libdef/@ginkgo-bioworks/react-json-schema-form-builder_v1.x.x/test_react-json-schema-form-builder_v1.x.x.js
@@ -5,6 +5,7 @@ import {
   FormBuilder,
   PredefinedGallery,
 } from '@ginkgo-bioworks/react-json-schema-form-builder';
+import type { CardBodyProps } from '@ginkgo-bioworks/react-json-schema-form-builder';
 import { it, describe } from 'flow-typed-test';
 
 describe('@ginkgo-bioworks/react-json-schema-form-builder', () => {
@@ -20,7 +21,54 @@ describe('@ginkgo-bioworks/react-json-schema-form-builder', () => {
     mods: {},
     className: 'foo'
   };
-
+  const propsWithMods = {
+    schema: '',
+    uischema: '',
+    onChange: (newSchema, newUiSchema) => {},
+    mods: {
+      customFormInputs: {
+        customFormInput0: {
+          displayName: 'custom form input 0',
+          defaultDataSchema: {
+            'type': 'number',
+          },
+          defaultUiSchema: {
+            'ui:widget': 'customFormInput0',
+          },
+          type: 'number',
+          cardBody: (props: CardBodyProps) => <div/>,
+          modalBody: (props: CardBodyProps) => <div/>,
+          matchIf: [{
+            types: ['number'],
+            widget: 'customFormInput0',
+          }],
+        },
+        customFormInput1: {
+          displayName: 'custom form input 1',
+          defaultDataSchema: {
+            'type': 'string',
+          },
+          defaultUiSchema: {
+            'ui:field': 'customFormInput1',
+          },
+          type: 'string',
+          cardBody: (props: CardBodyProps) => <div/>,
+          modalBody: (props: CardBodyProps) => <div/>,
+          matchIf: [{
+            types: ['string'],
+            field: 'customFormInput1',
+          }],
+        },
+      },
+      tooltipDescriptions: {
+        add: 'add text',
+        cardObjectName: 'card object name text',
+        cardDisplayName: 'card display name text',
+        cardDescription: 'card description text',
+        cardInputType: 'card input type text',
+      },
+    },
+  };
   const extraneousProps = {
     schema: '',
     uischema: '',
@@ -46,6 +94,10 @@ describe('@ginkgo-bioworks/react-json-schema-form-builder', () => {
     <FormBuilder {...optionalProps} />
   });
 
+  it('render the form builder with props with mods', () => {
+    <FormBuilder {...propsWithMods} />
+  });
+
   it('form builder errors on extraneous properties passed in', () => {
     // $FlowExpectedError
     <FormBuilder {...extraneousProps} />
@@ -60,9 +112,13 @@ describe('@ginkgo-bioworks/react-json-schema-form-builder', () => {
     <PredefinedGallery {...props} />
   });
 
-  it('render predefined with optional props', () => {
+  it('render predefined gallery with optional props', () => {
     <PredefinedGallery {...optionalProps} />
   });
+
+  it('render predefined gallery with mods in props', () => {
+    <PredefinedGallery {...propsWithMods}/>
+  })
 
   it('predefined gallery errors on extraneous properties passed in', () => {
     // $FlowExpectedError
