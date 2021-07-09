373087c1e898d58969fdd54363ab8a73fc0466ec
Cyril Nxumalo <cyril@docfox.io>
2021-07-09 12:31:56 +0200

    Add documentation on how to handle mods for placeholder field

--- src/formBuilder/types.js
+++ src/formBuilder/types.js
@@ -142,6 +142,7 @@ export type Mods = {|
     displayNameLabel?: string,
     descriptionLabel?: string,
     inputTypeLabel?: string,
+    placeHolderLabel?: string,
   |},
   showFormHead?: boolean,
   deactivatedFormInputs?: Array<string>,
