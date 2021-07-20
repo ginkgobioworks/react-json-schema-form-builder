abbf11f9ba180bea834f67976a6556c751d011f3
Monwabisi Sifumba <mo@Monwabisis-MacBook-Pro.local>
2021-07-20 15:24:56 +0200

    :fire: Remove preset column size values

--- src/formBuilder/utils.js
+++ src/formBuilder/utils.js
@@ -1653,35 +1653,6 @@ export function getNewElementDefaultDataOptions(
   }
 }
 
-export function availableColumnSizes(
-  inputColumnSizes: Array<number>,
-): Array<Object> {
-  const validInputColumnSizes = validateInputColumnSizes(inputColumnSizes);
-  const columnSizes = validInputColumnSizes || DEFAULT_COLUMN_SIZES;
-  return columnSizes;
-}
-
-const validateInputColumnSizes = (inputColumnSizes) => {
-  if (inputColumnSizes.length < 1) return;
-  if (!inputColumnSizes.every((size) => Number(size) === size)) return;
-  return inputColumnSizes.sort();
-};
-
-const DEFAULT_COLUMN_SIZES = [
-  { label: 1, value: 1 },
-  { label: 2, value: 2 },
-  { label: 3, value: 3 },
-  { label: 4, value: 4 },
-  { label: 5, value: 5 },
-  { label: 6, value: 6 },
-  { label: 7, value: 7 },
-  { label: 8, value: 8 },
-  { label: 9, value: 9 },
-  { label: 10, value: 10 },
-  { label: 11, value: 11 },
-  { label: 12, value: 12 },
-];
-
 export function getRandomId(): string {
   const chars = [
     'a',
