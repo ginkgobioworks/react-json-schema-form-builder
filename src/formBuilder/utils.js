1b489ffafd2c6c0c02085233d009c9bf0f05a349
Sabantu <msabantu@yahoo.com>
2021-09-14 15:39:59 +0200

    Ensure a section Column Size is not subsequently overwritten when a child card's column size is configured.

--- src/formBuilder/utils.js
+++ src/formBuilder/utils.js
@@ -864,12 +864,9 @@ export function updateSchemas(
   },
 ) {
   const { schema, uischema, onChange, definitionUi } = parameters;
-  const definedUi = (uischema || {}).definitions
-    ? { definitions: uischema.definitions }
-    : {};
 
   const newUiSchema = Object.assign(
-    definedUi,
+    { ...uischema },
     generateUiSchemaFromElementProps(elementArr, definitionUi),
   );
   const newSchema = Object.assign(
@@ -879,7 +876,6 @@ export function updateSchemas(
 
   // mandate that the type is an object if not already done
   newSchema.type = 'object';
-
   onChange(newSchema, newUiSchema);
 }
 
