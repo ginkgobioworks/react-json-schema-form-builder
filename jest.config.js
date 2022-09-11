298c20118cbe094a9a6d0236e32c323d25812655
Raymond Lam <ray@lam-ray.com>
2022-09-10 22:40:21 -0400

    Add support for React 18

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/example/"],
+  setupFiles: ["./src/jestShim.js"],
   setupFilesAfterEnv: ["./src/setupTests.js"], 
   transform: {
       "\\.[jt]sx?$": "babel-jest"
