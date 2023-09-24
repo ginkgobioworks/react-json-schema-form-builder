bcb48b465b87ac9e979b9beefb1165a2da22a755
Raymond Lam <rlam@ginkgobioworks.com>
2023-09-24 19:57:47 -0400

    Use Next.js in example

---
+++ example/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlaygroundContainer from '../components/PlaygroundContainer';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return <PlaygroundContainer title='React JSON Schema Form Builder' />;
+};
+
+export default App;
--- example/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PlaygroundContainer from './PlaygroundContainer';
-
-function App() {
-  return <PlaygroundContainer title='React JSON Schema Form Builder' />;
-}
-
-export default App;
